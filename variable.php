afe4f2cb19cca0eaf1c210a051803f05e3b08daf
Alighufron-28 <ghufronmga28@gmail.com>
2022-03-09 23:48:33 +0700

    Tugas 1, menambah file variable.php

---
+++ variable.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>pengoperasian aritmatika</title>
+    <style>
+        body {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    echo "Di Ketahui : <br>";
+    echo "x = 12<br>";
+    echo "y = 10<br>";
+    echo "z = 5<br><br>";
+    $x = 12;
+    $y = 10;
+    $z = 5;
+
+    echo "Hasil Penjumlahan Antara x, y dan z <br>";
+    $penjumlahan = $x + $y + $z;
+        echo "adalah = $penjumlahan<br><br>";
+
+    echo "Hasil Pengurangan Antara x, y dan z <br>";
+    $pengurangan  = $x - $y - $z;
+        echo "adalah = $pengurangan<br><br>";
+
+    echo "Hasil Perkalian Antara x, y dan z <br>";
+    $perkalian = $x * $y * $z;
+        echo "adalah = $perkalian";
+?>
+</body>
+</html>
