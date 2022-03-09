bb26410e09917eda923b6faed95b420861c3ea77
Alighufron-28 <ghufronmga28@gmail.com>
2022-03-09 23:51:19 +0700

    Tugas 1, menambah file function.php

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function Ljajargenjang($alas,$tinggi) {
+    $rumus = $alas * $tinggi;
+    echo "hasil = $rumus";
+}
+echo "1.) Di ketahui alas jajargenjang adalah 12 dan tingginya 5<br>";
+echo "Hitunglah luas jajargenjang tersebut<br>";
+Ljajargenjang(12,5);
+echo "<br><br>";
+
+echo "2.) Di ketahui alas jajargenjang adalah 17 dan tingginya 12<br>";
+echo "Hitunglah luas jajargenjang tersebut<br>";
+Ljajargenjang(17,12);
+echo "<br><br>";
+
+echo "3.) Di ketahui alas jajargenjang adalah 9 dan tingginya 13<br>";
+echo "Hitunglah luas jajargenjang tersebut<br>";
+Ljajargenjang(9,13);
+?>
