48906a09d7cdd04017ff563aea2771ba7b460997
Alighufron-28 <ghufronmga28@gmail.com>
2022-03-09 23:45:46 +0700

    Tugas 1, menambah file hello.php

---
+++ hello.php
@@ -0,0 +1,17 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>profile</title>
+</head>
+<body style="font-size: 100px;">
+<!-- syntax center untuk memposisikan text di tengah web dan ukuran text 100px -->
+<center>
+    <?php
+        echo "Nama  : M. Ali Ghufron <br>";
+        // menampilkan nama dari mahasiswa
+        echo "NIM   : 200411100003";
+        // menampilkan nim dari mahasiswaa
+    ?>
+</center>
+</body>
+</html>
